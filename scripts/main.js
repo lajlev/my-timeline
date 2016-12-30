de5ef266d9d866eeb20dbf332b9c8409b1dc0cce
Michael Lajlev <lajlev@gmail.com>
2016-12-30 15:48:03 +0100

    Link boardgamenames

--- scripts/main.js
+++ scripts/main.js
@@ -13,7 +13,7 @@ $(function(){
     "http://c.laj.lv/parser/bgg_lajlev.php",
 
     function(data) {
-     $('.bgg-last-3-games').html(data.play[0].item['@attributes'].name + ", " + data.play[1].item['@attributes'].name + " & " + data.play[2].item['@attributes'].name);
+     $('.bgg-last-3-games').html("<a target='_blank' href='https://boardgamegeek.com/boardgame/" + data.play[0].item['@attributes'].objectid + "'>" + data.play[0].item['@attributes'].name + "</a>, <a target='_blank' href='https://boardgamegeek.com/boardgame/" + data.play[1].item['@attributes'].objectid + "'>" + data.play[1].item['@attributes'].name + "</a> & <a target='_blank' href='https://boardgamegeek.com/boardgame/" + data.play[2].item['@attributes'].objectid + "'>" + data.play[2].item['@attributes'].name + "</a>");
     }
   );
 
