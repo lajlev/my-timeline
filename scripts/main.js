c31e3ee19b76d58c0819b14d315876257b5fe6f9
Michael Lajlev <lajlev@gmail.com>
2017-01-02 10:24:14 +0100

    add last.fm + new favicon

--- scripts/main.js
+++ scripts/main.js
@@ -20,9 +20,6 @@ $(function(){
   );
 
   // IMDB ratings
-
-  $(document).ready(function(){
-
   $.ajax({
     url: 'https://api.apifier.com/v1/execs/xWm9tAS7HqbLJteig/results?simplified=1',
     dataType: 'json',
@@ -30,56 +27,72 @@ $(function(){
       var randomObject = data[Math.floor(Math.random() * data.length)];
       var randomObjectId = randomObject.id;
 
-        $.ajax({
-          url: 'http://omdbapi.com/?i=tt'+ randomObjectId +'&apikey=8d6c247f',
-          dataType: 'json',
-          success: function(data) {
-            var imdbMyRating = randomObject.my_rating;
-            var imdbMyRatingWord;
-            var imdbTimeAgo = randomObject.date;
-            var imdbId = data.imdbID;
-            var imdbType = data.Type;
-            var imdbYear = data.Year;
-            var imdbPlot = data.Plot;
-            var imdbTitle = data.Title;
-            var currentDate = new Date();
-            var currentYear = currentDate.getFullYear();
-            // Word rating
-            if(imdbMyRating < 2) {
-              imdbMyRatingWord = "a crapy";
-            } else if(imdbMyRating < 5){
-              imdbMyRatingWord = "a sloopy";
-            } else if(imdbMyRating < 6){
-              imdbMyRatingWord = "a decent";
-            } else if(imdbMyRating < 7){
-              imdbMyRatingWord = "a pretty good";
-            } else if(imdbMyRating < 8){
-              imdbMyRatingWord = "a great";
-            } else if(imdbMyRating < 9){
-              imdbMyRatingWord = "an awesome";
-            } else {
-              imdbMyRatingWord = "a briliant";
-            }
-
-            if(imdbType == "series") {
-              imdbType = "TV serie";
-            }
-
-            // Improve copy if imdbTimeAgo don't contain time ago
-            if(!((imdbTimeAgo.substr(imdbTimeAgo.length - 3)) == "ago")) {
-              var imdbTimeYear = imdbTimeAgo.substr(imdbTimeAgo.length - 4);
-              var imdbTimeAgoYear = currentYear - imdbTimeYear;
-              imdbTimeAgo = imdbTimeAgoYear + ' years ago';
-            }
-            // Composed imdb string
-            $('.imdb-random-movie').html("a " +imdbType + " I watched "+imdbTimeAgo + ". It's called <a target='_blank' href='http://www.imdb.com/title/" + imdbId + "' title='I rated "+imdbTitle+" "+imdbMyRating+" out of 10.'>" + imdbTitle + "</a> " + imdbMyRatingWord + " " + imdbType + " from " + imdbYear + ". Basicly it's about: " + imdbPlot);
+      $.ajax({
+        url: 'http://omdbapi.com/?i=tt'+ randomObjectId +'&apikey=8d6c247f',
+        dataType: 'json',
+        success: function(data) {
+          var imdbMyRating = randomObject.my_rating;
+          var imdbMyRatingWord;
+          var imdbTimeAgo = randomObject.date;
+          var imdbId = data.imdbID;
+          var imdbType = data.Type;
+          var imdbYear = data.Year;
+          var imdbPlot = data.Plot;
+          var imdbTitle = data.Title;
+          var currentDate = new Date();
+          var currentYear = currentDate.getFullYear();
+          // Word rating
+          if(imdbMyRating < 2) {
+            imdbMyRatingWord = "a crapy";
+          } else if(imdbMyRating < 5){
+            imdbMyRatingWord = "a sloopy";
+          } else if(imdbMyRating < 6){
+            imdbMyRatingWord = "a decent";
+          } else if(imdbMyRating < 7){
+            imdbMyRatingWord = "a pretty good";
+          } else if(imdbMyRating < 8){
+            imdbMyRatingWord = "a great";
+          } else if(imdbMyRating < 9){
+            imdbMyRatingWord = "an awesome";
+          } else {
+            imdbMyRatingWord = "a briliant";
           }
-        });
+
+          if(imdbType == "series") {
+            imdbType = "TV serie";
+          }
+
+          // Improve copy if imdbTimeAgo don't contain time ago
+          if(!((imdbTimeAgo.substr(imdbTimeAgo.length - 3)) == "ago")) {
+            var imdbTimeYear = imdbTimeAgo.substr(imdbTimeAgo.length - 4);
+            var imdbTimeAgoYear = currentYear - imdbTimeYear;
+            imdbTimeAgo = imdbTimeAgoYear + ' years ago';
+          }
+          // Composed imdb string
+          $('.imdb-random-movie').html("a " +imdbType + " I watched "+imdbTimeAgo + ". It's called <a target='_blank' href='http://www.imdb.com/title/" + imdbId + "' title='I rated "+imdbTitle+" "+imdbMyRating+" out of 10.'>" + imdbTitle + "</a> " + imdbMyRatingWord + " " + imdbType + " from " + imdbYear + ". Basicly it's about: " + imdbPlot);
+        }
+      });
+    } // Success
+  }); //$.ajax
+
+  // Last.fm
+  $.ajax({
+    type : 'POST',
+    url : 'http://ws.audioscrobbler.com/2.0/',
+    data : 'method=user.gettopartists&' +
+           'user=lajlev&' +
+           'period=1month&' +
+           'limit=3&' +
+           'api_key=e122e0260c0153d515aca72b28cb91b3&' +
+           'format=json',
+    dataType : 'jsonp',
+    success : function(data) {
+      //console.log(data);
+      var artist = data.topartists.artist;
+      $('.lastfm-top3-month').html("<a href='"+artist[0].url+"'>" + artist[0].name + "</a>, <a href='"+artist[1].url+"'>" + artist[1].name + "</a> & <a href='"+artist[2].url+"'>" + artist[2].name + "</a>,"  );
+    }, // Success
+    error : function(code, message){
+      console.log("error");
     }
-  });
-
-});
-
-
-
-})
+  }); // $.ajax
+}); // $.Ready
